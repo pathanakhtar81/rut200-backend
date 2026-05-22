98797699db03ace26876a70bc3fad840cd25e4fd
pathanakhtar81 <pathanakhtar81@gmail.com>
2026-05-22 17:03:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,7 @@ def live():
     return jsonify(latest_data)
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8090, debug=False)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
